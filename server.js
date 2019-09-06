c76f0f3f66bd461f2f5b22bb58e0fd61c2847a1f
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-09-06 11:20:48 +0530

    Define routes

--- server.js
+++ server.js
@@ -13,6 +13,12 @@ connectDB()
 // GET request to API
 app.get('/', (req, res) => res.send('API running'))
 
+// Define routes 
+app.use('/api/users', require('./routes/api/users'))    // This will make '/api/users' pertain to '/' as defined in './routes/api/users.js'
+app.use('/api/profile', require('./routes/api/profile'))    // This will make '/api/profile' pertain to '/' as defined in './routes/api/profile.js'
+app.use('/api/auth', require('./routes/api/auth'))    // This will make '/api/auth' pertain to '/' as defined in './routes/api/auth.js'
+app.use('/api/posts', require('./routes/api/posts'))    // This will make '/api/posts' pertain to '/' as defined in './routes/api/posts.js'
+
 // Get the environment port variable
 // If no port variable available, it will work on port 5000
 const PORT = process.env.PORT || 5000
