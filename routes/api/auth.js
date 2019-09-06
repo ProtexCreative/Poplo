7dd088693b8a45b9e9e791fd7196c81385dac60c
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-09-06 16:08:38 +0530

    Username changed to Id

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,11 +2,21 @@
 const express = require('express')
 // Get express router
 const router = express.Router()
+const auth = require('../../middleware/auth')   // Get the auth middleware.
+
+const User = require('../../models/Users')
 
 // @route   GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth Route'))
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')   // Get the user from the id stored in token. Ignore password.
+        res.json(user)  // Print the user.
+    } catch (err) {
+        console.error(err.message)
+    }
+})
 
 // Export route
 module.exports = router
