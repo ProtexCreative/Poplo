f91c6b2de4a4b16868d8368ea55486ef5ff6373b
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-09-06 14:23:43 +0530

    User Model Created

---
+++ models/Users.js
@@ -0,0 +1,35 @@
+// Get Mongoose
+const mongoose = require('mongoose')
+
+// Create User Schema
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        lowercase,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+// Export the User Model
+module.exports = User = mongoose.model('User', UserSchema)
