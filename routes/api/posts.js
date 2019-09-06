0fa4355722e21989152075583d9f406b6b766fa1
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-09-06 11:18:35 +0530

    posts.js setup

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,10 +3,10 @@ const express = require('express')
 // Get express router
 const router = express.Router()
 
-// @route   GET api/post
+// @route   GET api/posts
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Post Route'))
+router.get('/', (req, res) => res.send('Posts Route'))
 
 // Export route
 module.exports = router
