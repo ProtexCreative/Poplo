f7007a834dae441d19ee5e5b6baf6c18b39d838e
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-09-06 17:59:30 +0530

    Login Route Created

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,19 +2,61 @@
 const express = require('express')
 // Get express router
 const router = express.Router()
+// Get jsonwebtoken
+const jwt = require('jsonwebtoken')
+// Get bcrypt to hash the password
+const bcrypt = require('bcryptjs')
+// Get express validator to validate the entries 
+const { check, validationResult } = require('express-validator')
+// Get the config file
+const config = require('config')
 const auth = require('../../middleware/auth')   // Get the auth middleware.
 
 const User = require('../../models/Users')
 
-// @route   GET api/auth
-// @desc    Test route
+// @route   POST api/auth
+// @desc    Authenticate user & get Token
 // @access  Public
-router.get('/', auth, async (req, res) => {
+router.post('/', [
+    check('password', 'Password is required.').exists(),   // Password should be of a minimum length. 
+    check('username', 'Username should be lowercase.').isLowercase()    // Username should be lowercase.
+], async (req, res) => {
+    const errors = validationResult(req)    // Get all the errors from the request using validationResult
+
+    if (!errors.isEmpty()) {    // If the errors array is not empty.
+        return res.status(400).json({ errors: errors.array() })     // Output the errors array.
+    }
+
+    const { username, password } = req.body    // Destructure the req.body Object
+
     try {
-        const user = await User.findById(req.user.id).select('-password')   // Get the user from the id stored in token. Ignore password.
-        res.json(user)  // Print the user.
+        let user = await User.findOne({ username })    // Get the user from the username.
+
+        if (!user) {     // If the user exists.
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials.' }] })
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password)   // Compare the entered plain password with the hashed password in the database.
+
+        if (!isMatch) {   // If password does not match.
+            return res.status(400).json({ errors: [{ msg: 'Invalid Credentials.' }] })
+        }
+
+        const payload = {   // Create a payload object to send in token.
+            user: {
+                id: user.id     //  Get the id from user object.
+            }
+        }
+
+        // TODO: Change the expiration time at the time of production.
+        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000000 },     // Sign the token, set the jwtSecret, set an expiration time of token
+            (err, token) => {
+                if (err) throw err  // If error available, throw error.
+                res.json({ token })     // Response object will have a token.
+            })
     } catch (err) {
-        console.error(err.message)
+        console.log(err.message)
+        res.status(500).send('Server Error!')
     }
 })
 
