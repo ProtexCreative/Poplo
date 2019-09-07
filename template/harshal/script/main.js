a90f1e3b1cdb3f9563af9390aa9ae1a8d75b8c75
HarshalKudale <harshalkudale56@gmail.com>
2019-09-07 17:29:20 +0530

    changs

--- template/harshal/script/main.js
+++ template/harshal/script/main.js
@@ -1,37 +1,12 @@
-// DOM Elements
-const time = document.getElementById('time');
-
-
-// Options
-const showAmPm = true;
-
-// Show Time
-function showTime() {
-  let today = new Date(),
-    hour = today.getHours(),
-    min = today.getMinutes(),
-    sec = today.getSeconds();
-
-  // Set AM or PM
-  const amPm = hour >= 12 ? 'PM' : 'AM';
-
-  // 12hr Format
-  hour = hour % 12 || 12;
-
-  // Output Time
-  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${showAmPm ? amPm : ''}`;
-  setTimeout(showTime, 1000);
+var myNav = document.getElementById('navbar');
+window.onscroll = function () { 
+    "use strict";
+    if (document.body.scrollTop >= 100vh ) {
+        myNav.classList.add("navbar-colored");
+        myNav.classList.remove("navbar-home");
+    } 
+    else {
+        myNav.classList.add("navbar-home");
+        myNav.classList.remove("navbar-colored");
+    }
 }
-
-// Add Zeros
-function addZero(n) {
-  return (parseInt(n, 10) < 10 ? '0' : '') + n;
-}
-
-
-
-
-// Run
-showTime();
-
-
