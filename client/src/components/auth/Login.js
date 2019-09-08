23268ef3ec4c5a2d8d60b8629735696edecffdea
Pratik Temkar <pratiktemkarofficial@gmail.com>
2019-09-08 21:48:39 +0530

    State Setup

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,24 +1,36 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 
 const Login = () => {
+    const [formData, setFormData] = useState({
+        username: '',
+        password: ''
+    })
+
+    const { username, password } = formData
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    const onSubmit = e => {
+
+    }
+
     return (
         <Fragment>
             <h1>Login</h1>
             <form>
-                <div className="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                <div class="form-group">
+                    <label >Username</label>
+                    <input type="text" class="form-control" name='username' value={username} onChange={e => onChange(e)} placeholder="Enter username" />
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={password} onChange={e => onChange(e)} placeholder="Password" />
                 </div>
                 <div className="form-group form-check">
                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                    <label className="form-check-label" for="exampleCheck1">Remember Me</label>
                 </div>
-                <button type="submit" className="btn btn-primary">Submit</button>
+                <button type="submit" className="btn btn-primary">Login</button>
             </form>
         </Fragment>
     )
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,24 +1,58 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name: '',
+        username: '',
+        email: '',
+        password: '',
+        password2: ''
+    })
+
+    const { name, email, username, password, password2 } = formData
+
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value })
+
+    const onSubmit = e => {
+        e.preventDefault()
+        if (password != password2) {
+            console.log('pas')
+        } else {
+            console.log(formData)
+        }
+    }
+
     return (
         <Fragment>
             <h1>Register</h1>
-            <form>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            <form onSubmit={e => onSubmit(e)}>
+                <div className="form-group">
+                    <label >Full Name</label>
+                    <input type="text" className="form-control" name='name' value={name} onChange={e => onChange(e)} placeholder="Enter Full Name" required />
                 </div>
-                <div class="form-group">
-                    <label for="exampleInputPassword1">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" />
+                <div className="form-group">
+                    <label >Username</label>
+                    <input type="text" className="form-control" name='username' value={username} onChange={e => onChange(e)} placeholder="Enter username" required />
                 </div>
-                <div class="form-group form-check">
-                    <input type="checkbox" class="form-check-input" id="exampleCheck1" />
-                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <div className="form-group">
+                    <label >Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name='email' value={email} onChange={e => onChange(e)} placeholder="Enter email" />
+                    <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div className="form-group">
+                    <label >Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={password} onChange={e => onChange(e)} placeholder="Password" />
+                </div>
+                <div className="form-group">
+                    <label >Confirm Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword2" name='password2' value={password2} onChange={e => onChange(e)} placeholder="Password" />
+                </div>
+                <div className="form-group form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" for="exampleCheck1">I agree to the terms and conditions. <Link to="#">Read T&C</Link></label>
+                </div>
+                <button type="submit" className="btn btn-primary">Register</button>
             </form>
         </Fragment>
     )
